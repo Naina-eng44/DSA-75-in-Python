44eecfffaa4695d0c7fb73fe1c612e2b4c66e84b
Naina-eng44 <chauhannaina2005@gmail.com>
2026-06-30 21:32:38 +0530

    Solved Container with Most Water Problem

--- two_pointer.py
+++ two_pointer.py
@@ -43,3 +43,33 @@ class Solution:
 obj = Solution()
 print(obj.isSubsequence("ace", "abcde"))  # Output: True
 print(obj.isSubsequence("aec", "abcde"))  # Output: False
+
+
+
+# Leetcode 11. Container With Most Water
+# Difficulty: Medium    Topic: Array, Two Pointers  
+# Time complexity: O(n) where n is the length of the array, space complexity: O(1)
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        left = 0
+        right = len(height) - 1
+
+        maximum = 0
+
+        while left < right:
+            width = right - left
+            area = min(height[left], height[right]) * width
+            maximum = max(maximum, area)
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return maximum
+
+obj = Solution()
+height = [1,8,6,2,5,4,8,3,7]
+print(obj.maxArea(height))  # Output: 49
